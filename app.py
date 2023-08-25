a4b21cac3b24f335296b69c9a6c1a71c4be2bef0
Mohsina-Ahmed <mohsinaahmedchowdhury@gmail.com>
2023-08-25 16:42:05 +0100

    Adding the domain name that works with exoframe

--- app.py
+++ app.py
@@ -7,20 +7,20 @@ app = Flask(__name__)
 def get_home():
     return "I am CI/CD hero!"
 
-# if __name__ == '__main__':
-#     app.run(
-#       debug=True,
-#       host="0.0.0.0" # Listen for connections _to_ any server
-#     )
+if __name__ == '__main__':
+    app.run(
+      debug=True,
+      host="0.0.0.0" # Listen for connections _to_ any server
+    )
 
 # if __name__ == '__main__':
 #   app.run(debug=True)
 
-if __name__ == '__main__':
-    # We also run the server differently depending on the environment.
-    # In production we don't want the fancy error messages — users won't know
-    # what to do with them. So no `debug=True`
-    if os.environ.get("APP_ENV") == "PRODUCTION":
-        app.run(port=5000, host='0.0.0.0')
-    else:
-        app.run(debug=True, port=5000, host='0.0.0.0')
+# if __name__ == '__main__':
+#     # We also run the server differently depending on the environment.
+#     # In production we don't want the fancy error messages — users won't know
+#     # what to do with them. So no `debug=True`
+#     if os.environ.get("APP_ENV") == "PRODUCTION":
+#         app.run(port=5000, host='0.0.0.0')
+#     else:
+#         app.run(debug=True, port=5000, host='0.0.0.0')
